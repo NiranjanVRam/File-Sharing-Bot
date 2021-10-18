4db91692dcebce1c27f83fe9ca5d5c366c652783
Niranjan V Ram <83854040+NiranjanVRam@users.noreply.github.com>
2021-10-18 18:24:52 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>LiqWid</a>\n\n○ Language : <code>Python3</code>\n\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n\n○ MOVIES : <a href='https://t.me/fhmoviechat'>FH MOVIES</a>\n\n\n\n○ SERIES : <a href='https://t.me/fhserieschat'>FH SERIES</a>\n\n○ CHANNEL : <a href='https://t.me/fileshomeofficial'>FH GROUP</a>\n\n○ SUPPORT : <a href='https://t.me/fhhelperbot'>Click Here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
